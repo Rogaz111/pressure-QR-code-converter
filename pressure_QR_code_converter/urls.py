c812786755c1851113899b5468fefda692f7cd97
Rayn Daries <rayn@1stream.co.za>
2024-11-20 14:46:22 +0200

    Amendments to settings to include media dir. Amendments to urls to add static path.

--- pressure_QR_code_converter/urls.py
+++ pressure_QR_code_converter/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from my_app import views
+from my_app.views import generate_qr_code
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Default admin site URL
@@ -8,4 +11,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('capture_pressure/', views.capture_pressure, name='pressures'),
     path('import_view/', views.import_view, name='imports'),
+    path('generate_qr_code/', generate_qr_code, name='generate_qr_code'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
